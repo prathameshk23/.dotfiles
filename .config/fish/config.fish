7a9c0f77ee3eb7e1015cb989731b6fa49608aadc
prathameshk23 <prathameshkulkarni952@gmail.com>
2023-04-27 17:37:20 +0530

    init

---
+++ .config/fish/config.fish
@@ -0,0 +1,31 @@
+if status is-interactive
+  alias v='nvim'
+  alias tl='tmux list-sessions'
+  alias ta='tmux a'
+  alias tk='tmux kill-server'
+  alias lg='lazygit'
+  alias s='startx'
+    # Commands to run in interactive sessions can go here
+end
+
+function fzf_tmux
+  cd
+  ./.config/tmux/tmux-sessionizer
+end
+
+function cheat
+  cd
+  ./.config/tmux/tmux-cht
+end
+
+bind \cf fzf_tmux
+bind \ch cheat
+
+
+
+fortune | cowsay | lolcat
+set --export PATH $HOME/.cargo/bin $PATH
+
+starship init fish | source
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/pratham/.ghcup/bin $PATH # ghcup-env
---
+++ README.md
@@ -0,0 +1,5 @@
+#Dotfiles
+
+These are my dotfiles 
+
+
