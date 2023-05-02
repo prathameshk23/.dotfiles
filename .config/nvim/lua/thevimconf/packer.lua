95f2efe476b57985a20ab1137151926cb78c60cf
prathameshk23 <prathameshkulkarni952@gmail.com>
2023-05-02 15:18:57 +0530

    Added copilot

--- .config/nvim/lua/thevimconf/packer.lua
+++ .config/nvim/lua/thevimconf/packer.lua
@@ -25,6 +25,7 @@ return require('packer').startup(function(use)
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
   use 'nvim-tree/nvim-tree.lua'
+  use 'github/copilot.vim'
 
   use {
     'VonHeikemen/lsp-zero.nvim',
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -120,6 +120,11 @@ _G.packer_plugins = {
     path = "/home/pratham/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
+  ["copilot.vim"] = {
+    loaded = true,
+    path = "/home/pratham/.local/share/nvim/site/pack/packer/start/copilot.vim",
+    url = "https://github.com/github/copilot.vim"
+  },
   ["flutter-tools.nvim"] = {
     loaded = true,
     path = "/home/pratham/.local/share/nvim/site/pack/packer/start/flutter-tools.nvim",
